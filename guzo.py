a5fc1a4317336d1e599ed72cf1e8c0fa4e066a8b
addiseshetu <addiseshetu@gmail.com>
2020-09-18 15:03:57 -0400

    added a new file

---
+++ guzo.py
@@ -0,0 +1,31 @@
+import time
+from multiprocessing import Process, Lock, Value
+
+
+def add_500_lock(total, lock):
+    for i in range(100):
+        time.sleep(0.01)
+        lock.acquire()
+        total.value += 5
+        lock.release()
+
+
+def sub_500_lock(total, lock):
+    for i in range(100):
+        time.sleep(0.01)
+        lock.acquire()
+        total.value -= 5
+        lock.release()
+if __name__ == '__main__':
+    
+    total = Value('i', 500)
+    lock = Lock()
+    add_proc = Process(target=add_500_lock, args=(total, lock))
+    sub_proc = Process(target=sub_500_lock, args=(total, lock))
+
+    add_proc.start()
+    sub_proc.start()
+
+    add_proc.join()
+    sub_proc.join()
+    print(total.value)
